a5d2b383859aeadb2e26dd2fc0a178c5b246f023
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-06-10 15:33:53 +0200

    Added suite file extracting

--- nimbo_app.rb
+++ nimbo_app.rb
@@ -3,18 +3,20 @@ require 'json'
 
 class NimboApp < Sinatra::Base
  
-  set :suites_dir, File.expand_path(File.join('..', File.dirname(__FILE__), 'suites'))
+  set :suites_dir, File.expand_path(File.join(File.dirname(__FILE__), '../suites'))
 
   get '/' do
     erb :home
   end
 
   post '/test' do
-    file_name = File.join(settings.suites_dir, params['suite_archive'][:filename])
-    File.open(file_name, "w") do |f|
-        f.write(params['suite_archive'][:tempfile].read)
-    end
+    suite_id = `uuid`
+    suite_dir = File.join settings.suites_dir, suite_id
+    archive = params['suite_archive'][:tempfile].path
+
+    system("mkdir #{suite_dir}")
+    system("tar -xf #{archive} -C #{suite_dir}")
+    
     { status: 'ok' }.to_json
   end
-
 end
